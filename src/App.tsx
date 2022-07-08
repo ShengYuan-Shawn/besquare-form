2f4e35b5b59bad31f1a0068027787193a0c9b706
Shawn Lim <lsy2pw@gmail.com>
2022-07-08 17:43:16 +0800

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,37 @@
-import * as React from 'react';
-import './style.css';
+import * as React from "react";
+import "./style.css";
 
 export default function App() {
-
-  let [name,setName] = React.useState<String>("");
-  const [age, setAge] = React.useState<String | undefined>();
+  const [name, setName] = React.useState<string>("");
+  const [age, setAge] = React.useState<string | undefined>();
 
   const changeName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
   };
 
   const changeAge = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
+    setAge(event.target.value);
   };
 
   return (
     <div>
-      <input name="Name" value={name} onChange={changeName} />
-      <input name="Age" value={age} onChange={changeAge} />
+      <Input name="Name" value={name} onChange={changeName} />
+      <Input name="Age" value={age} onChange={changeAge} />
     </div>
-  )
+  );
+}
 
-  type InputType = {
-    name: String;
-    value: String;
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
-  }
+type InputType = {
+  name: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
-  const Input = ({value, onChange, name)} => {
-  }
+const Input = ({ value, onChange, name }) => {
+  return (
+    <div>
+      <label>{name}</label>
+      <input value={value} onChange={onChange} />
+    </div>
+  );
+};
